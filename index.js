7a52dbb70a33d05159b8d18b15bc9022c8145e52
Gerardo Perez <198660767+gperezunitec@users.noreply.github.com>
2025-05-14 21:00:00 -0600

    server added

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+import express from 'express';
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
